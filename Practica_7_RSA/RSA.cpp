2dc6e442a140047c4d67bf26c535eebb9a75e182
Pavel <nichitapavel@live.com>
2013-04-18 17:33:37 +0100

    Lehman-Peralta, define, descifrado
    Corregido error en Lehman Peralta, definidos el tamano del vector
    aleatorio y la base con variables, y implementado el descifrado a falta
    de comprobar los datos de Pino.

--- Practica_7_RSA/RSA.cpp
+++ Practica_7_RSA/RSA.cpp
@@ -10,6 +10,8 @@
 #include <algorithm>
 
 #define ull unsigned long long
+#define BASE 100
+#define TAMANOVECTORALEATORIO 100
 
 using namespace std;
 
@@ -33,7 +35,7 @@ bool LehmanPeraltaNoDivisible(ull numero){
 vector<ull> LehmanPeraltaRandom100(ull p){
 	vector<ull> array;
 	srand(time(NULL));
-	for (int i = 0; i < 100; ++i)
+	for (int i = 0; i < TAMANOVECTORALEATORIO; ++i)
 	{
 		array.push_back((random() % (p-1))+1);
 	}
@@ -41,24 +43,24 @@ vector<ull> LehmanPeraltaRandom100(ull p){
 	return array;
 }
 
-vector<ull> LehmanPeraltaCalculoY(vector<ull> array_aleatorio, ull p){
-	vector<ull> array_y;
+vector<ull> LehmanPeraltaCalculoA(vector<ull> array_aleatorio, ull p){
+	vector<ull> array_a;
 	ull mitad_p = (p-1)/2;
-	for (int i = 0; i < 100; ++i)
+	for (int i = 0; i < BASE; ++i)
 	{
-		array_y.push_back(Exponente(array_aleatorio[i], mitad_p, p));
+		array_a.push_back(Exponente(array_aleatorio[i], mitad_p, p));
 	}
-	return array_y;
+	return array_a;
 }
 
 //Control del paso A de LehPer, para los 100 numeros del array, comprobar [a^((p-1)/2) mod p] == 1
 //si el resultado de todo ai, el numero no es primo
 //si existe al menos un ai distinto de 1 se pasa a siguiente paso
-//retorno FALSE si encuentra al menos un numero que no cumple [a^((p-1)/2) mod p] == 1
-bool LehmanPeraltaPasoA(vector<ull> array_aleatorio, vector<ull> array_y, ull p){
-	for (int i = 0; i < 100; ++i)
+//retorno FALSE si encuentra al menos un numero que cumple [a^((p-1)/2) mod p] != 1
+bool LehmanPeraltaPasoA(vector<ull> array_aleatorio, vector<ull> array_a, ull p){
+	for (int i = 0; i < TAMANOVECTORALEATORIO; ++i)
 	{
-		if (array_aleatorio[i] != array_y[i])
+		if (array_a[i] != 1)
 		{
 			return false;
 		}
@@ -67,11 +69,12 @@ bool LehmanPeraltaPasoA(vector<ull> array_aleatorio, vector<ull> array_y, ull p)
 }
 
 //Control paso B de LehPer, para los 100 numeros del array, comprobar que estan dentro del rango (1, p-1)
-//retorna FALSE si encuentra un numero que esta fuera del rango
-bool LehmanPeraltaPasoB(vector<ull> array_y, ull p){
-	for (int i = 0; i < 100; ++i)
+//retorna FALSE si encuentra un numero que no es ni 1 ni p-1, ej. p=12, i = 6 retorno true porque
+//i != 1 or i != p-1(11)
+bool LehmanPeraltaPasoB(vector<ull> array_a, ull p){
+	for (int i = 0; i < TAMANOVECTORALEATORIO; ++i)
 	{
-		if (array_y[i] < 1 or array_y[i] > p-1)
+		if (array_a[i] > 1 and array_a[i] < p-1)
 		{
 			return false;
 		}
@@ -82,14 +85,40 @@ bool LehmanPeraltaPasoB(vector<ull> array_y, ull p){
 //Control paso C de LehPer, primer paso B tiene que devolver TRUE
 //si hay al menos un numero que es igual a p-1, entonces p es primo
 //devuelve TRUE si p es primo
-bool LehmanPeraltaPasoC(vector<ull> array_y, ull p){
-	for (int i = 0; i < 100; ++i)
+bool LehmanPeraltaPasoC(vector<ull> array_a, ull p){
+	for (int i = 0; i < TAMANOVECTORALEATORIO; ++i)
 	{
-		if (array_y[i] == p-1)
+		if (array_a[i] == p-1)
 		{
 			return true;
 		}
 	}
+	return false;
+}
+
+//Contenedor de todo el algoritmo de Lehman Peralta
+//Devuelve TRUE si numero p es primo
+bool LehmanPeraltaContenedor(ull p){
+	vector<ull> array_aleatorio, array_y;
+	
+	if (p == 2 or p == 3 or p == 5 or p == 7 or p == 11)
+	{
+		return true;
+	}
+
+	if (LehmanPeraltaNoDivisible(p) == true)
+	{
+		array_aleatorio = LehmanPeraltaRandom100(p);
+		array_y = LehmanPeraltaCalculoA(array_aleatorio, p);
+		if (LehmanPeraltaPasoA(array_aleatorio, array_y, p) == false)
+		{
+			if (LehmanPeraltaPasoB(array_y, p) == true)
+			{
+				return LehmanPeraltaPasoC(array_y, p);
+			}
+		}
+	} 
+	
 	return false;
 }
 
@@ -107,35 +136,9 @@ ull Exponente(ull a, ull p, ull m){
 	return expo;
 }
 
-//Contenedor de todo el algoritmo de Lehman Peralta
-//Devuelve TRUE si numero p es primo
-bool LehmanPeraltaContenedor(ull p){
-	vector<ull> array_aleatorio, array_y;
-	
-	if (p == 2 or p == 3 or p == 5 or p == 7 or p == 11)
-	{
-		return true;
-	}
-
-	if (LehmanPeraltaNoDivisible(p) == true)
-	{
-		array_aleatorio = LehmanPeraltaRandom100(p);
-		array_y = LehmanPeraltaCalculoY(array_aleatorio, p);
-		if (LehmanPeraltaPasoA(array_aleatorio, array_y, p) == false)
-		{
-			if (LehmanPeraltaPasoB(array_y, p) == true)
-			{
-				return LehmanPeraltaPasoC(array_y, p);
-			}
-		}
-	} 
-	
-	return false;
-}
-
 void EuclidesExtendido(ull f_n, ull d, ull& inverso, ull& exponente_modular){
 	ull a = f_n, b = d, modulo = 1, a_2 = f_n;
-	long long unidad_negativa = -1, z3, z1 = 0, z2 = 1;
+	long long z3, z1 = 0, z2 = 1;
 	
 	
 	if (b > a)
@@ -164,10 +167,19 @@ void EuclidesExtendido(ull f_n, ull d, ull& inverso, ull& exponente_modular){
 		z3 += a_2;
 	}
 	//cout << "Resultado final: " << "Inverso: " << z3 << " mcd(): " << modulo << endl;
-	inverso = (ull)(z3);
-	exponente_modular = (ull)(b);
+	
+	if (b != 1)
+	{
+		inverso = (ull)(0);
+		exponente_modular = (ull)(0);
+	}
+	else
+	{
+		inverso = (ull)(z3);
+		exponente_modular = (ull)(b);
+		cout << "E: " << inverso << endl;
+	}
 	//cout << "Resultado final: " << "Inverso: " << inverso << " mcd(): " << exponente_modular << endl;
-	cout << "E: " << inverso << endl;
 }
 
 string GetMessage(void){
@@ -215,7 +227,7 @@ vector<ull> RSACifrar(string mensaje, ull e, ull n){
 		for (int j = 0; j < 4; ++j)
 		{
 			caracter_en_base100 = (ull)(mensaje[i+j])-65;
-			mensaje_base_100 += caracter_en_base100*pow(100, 3-j);
+			mensaje_base_100 += caracter_en_base100*pow(BASE, 3-j);
 			//cout << mensaje_base_100 << endl;
 		}
 		//cout << endl;
@@ -235,21 +247,80 @@ void VerMensajeCifrado(vector<ull> mensaje_cifrado){
 }
 
 void RSADescifrar(vector<ull> mensaje_cifrado, ull d, ull n){
-	vector<ull> mensaje_descifrado;
+	//ull input, a, base;
+	ull input = mensaje_cifrado[0], a, base;
 	for (int i = 0; i < mensaje_cifrado.size(); ++i)
 	{
-		mensaje_descifrado.push_back(Exponente(mensaje_cifrado[i], d, n));
+		//input = Exponente(mensaje_cifrado[i], d, n);
+		for (int i = 0; i < 3; ++i)
+		{
+			base = pow(BASE, 3-i);
+			a = input/base;
+			input = input % base;
+			cout << (char)(a + 65);
+		}
+		cout << (char)(input + 65);
 	}
-	VerMensajeCifrado(mensaje_descifrado);
+	cout << endl;
 }
 
 int main(int argc, char const *argv[])
 {
+	// Ejemplo 1
 	
 	ull p = 421, q = 7, n = p*q, f_n = (p-1) * (q-1), d = 1619;
+	ull inverso, exponente_modular;
 	string mensaje = "MANDADINEROS";
-	//ull p = 12347, q = 92347, n = p*q, f_n = (p-1) * (q-1), d = 5;	
-	//string mensaje = "AMIGOMIO";
+	vector<ull> mensaje_cifrado;
+	
+	// Ejemplo	2
+	/*
+	ull p = 12347, q = 92347, n = p*q, f_n = (p-1) * (q-1), d = 5;	
+	ull inverso, exponente_modular;
+	string mensaje = "AMIGOMIO";
+	vector<ull> mensaje_cifrado;
+
+	/*
+	vector<ull> mensaje_cifrado;
+	ull p, q, d;
+
+	cout << "Dame P:";
+	cin >> p;
+	cout << "Dame Q:";
+	cin >> q;
+ 	cout << "Dame D:";
+	cin >> d;
+
+	ull n = p*q, f_n = (p-1) * (q-1), inverso, exponente_modular;
+	
+	string mensaje = GetMessage();
+	*/
+	cout << mensaje << endl;
+	cout << "P: " << p << endl;
+	cout << "Q: " << q << endl;
+	cout << "D: " << d << endl;
+	cout << "N: " << n << endl;
+	cout << "F(n): " << f_n << endl;
+	
+	
+	if (LehmanPeraltaContenedor(p) and LehmanPeraltaContenedor(q))
+	{
+		EuclidesExtendido(f_n, d, inverso, exponente_modular);
+		if (inverso == 0 and exponente_modular == 0)
+			cout << "D no tiene inverso!" << endl;
+		else
+		{
+			mensaje_cifrado = RSACifrar(mensaje, inverso, n);
+			cout << "Mensaje cifrado: ";
+			VerMensajeCifrado(mensaje_cifrado);
+		}
+	}
+	
+
+	//vector<ull> mensaje_cifrado(1, 12001303);
+	//cout << "Mensaje descifrado: ";
+	//RSADescifrar(mensaje_cifrado, d, n);
+
 	/*
 	ull p_2 = 885320963;
 	ull p_3 = 61;
@@ -263,13 +334,10 @@ int main(int argc, char const *argv[])
 	ull p_11 = 5;
 	ull p_12 = 7;
 	ull p_13 = 11;
-
-	
-	bool p_es_primo = LehmanPeraltaContenedor(p);
-	cout << p_es_primo << endl;
+	ull p_14 = 200;
+	ull p_15 = 1525;
 	
 	cout << "P2: " << LehmanPeraltaContenedor(p_2) << endl;
-	
 	cout << "P3: " << LehmanPeraltaContenedor(p_3) << endl;
 	cout << "P4: " << LehmanPeraltaContenedor(p_4) << endl;
 	cout << "P5: " << LehmanPeraltaContenedor(p_5) << endl;
@@ -281,32 +349,8 @@ int main(int argc, char const *argv[])
 	cout << "P11: " << LehmanPeraltaContenedor(p_11) << endl;
 	cout << "P12: " << LehmanPeraltaContenedor(p_12) << endl;
 	cout << "P13: " << LehmanPeraltaContenedor(p_13) << endl;
+	cout << "P14: " << LehmanPeraltaContenedor(p_14) << endl;
+	cout << "P15: " << LehmanPeraltaContenedor(p_15) << endl;
 	*/
-
-	//ull peta = 885320963, qutoa = 238855417, d = 1164402471153538991;
-	//ull a = 19, fn = 12;
-	//ull exponente_modular_1, inverso_1, f_n = (peta-1) * (qutoa-1);
-	//ull a_2 = 2753, fn_2 = 3120;
-	//ull a_3 = 2753, fn_3 = 3120;
-	//ull a_5 = 421;
-	
-	ull inverso, exponente_modular;
-	cout << "P: " << p << endl;
-	cout << "Q: " << q << endl;
-	cout << "D: " << d << endl;
-	cout << "N: " << n << endl;
-	cout << "F(n): " << f_n << endl;
-	cout << mensaje << endl;
-
-
-	EuclidesExtendido(d, f_n, inverso, exponente_modular);
-
-	//string mensaje = GetMessage();
-
-	vector<ull> mensaje_cifrado = RSACifrar(mensaje, inverso, n);
-
-	VerMensajeCifrado(mensaje_cifrado);
-
-	RSADescifrar(mensaje_cifrado, d, n);	
 	return 0;
 }

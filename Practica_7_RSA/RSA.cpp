1b7833810d676193aa805206757c431e4645c3a2
Pavel <nichitapavel@live.com>
2013-04-19 18:06:12 +0100

    Final, cambio de "AMIG" a "AMIGOMIO"

--- Practica_7_RSA/RSA.cpp
+++ Practica_7_RSA/RSA.cpp
@@ -284,7 +284,7 @@ int main(int argc, char const *argv[])
 	/**/
 	ull p = 2347, q = 347, n = p*q, f_n = (p-1) * (q-1), d = 5;	
 	ull inverso, exponente_modular;
-	string mensaje = "AMIG";
+	string mensaje = "AMIGOMIO";
 	vector<ull> mensaje_cifrado;
 
 	/*
